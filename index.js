e87b934c83aa24c05eec29d2fe88d86ef5e164fb
raylopez509 <raylopez509@gmail.com>
2023-01-14 10:25:35 -0800

    Changed disabled buttons
    Now disabled buttons are still opaque and the text colors are the same so there's no change when you disable the button.

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ let turn = true; //true = x : false = O
 let Xsquares = new Array();
 let Osquares = new Array();
 
-function clickSquare(button, number) {
+function clickSquare(button) {
     if (turn) {
         document.getElementById(button).innerHTML = "X";
         document.getElementById(button).disabled = true;
-        Xsquares.push(number);
+        Xsquares.push(button);
         document.getElementById("winner").innerHTML = "It is O's Turn";
         checkWin(Xsquares);
         turn = false;
@@ -14,7 +14,7 @@ function clickSquare(button, number) {
     else {
         document.getElementById(button).innerHTML = "O";
         document.getElementById(button).disabled = true;
-        Osquares.push(number);
+        Osquares.push(button);
         document.getElementById("winner").innerHTML = "It is X's Turn"
         checkWin(Osquares);
         turn = true;
@@ -22,23 +22,32 @@ function clickSquare(button, number) {
 }
 
 function checkWin(squares) {
-    if ((squares.includes(1) && squares.includes(2) && squares.includes(3)) ||
-        (squares.includes(4) && squares.includes(5) && squares.includes(6)) ||
-        (squares.includes(7) && squares.includes(8) && squares.includes(9)) ||
-        (squares.includes(1) && squares.includes(4) && squares.includes(7)) ||
-        (squares.includes(2) && squares.includes(5) && squares.includes(8)) ||
-        (squares.includes(3) && squares.includes(6) && squares.includes(9)) ||
-        (squares.includes(1) && squares.includes(5) && squares.includes(9)) ||
-        (squares.includes(3) && squares.includes(5) && squares.includes(7))) {//win conditions
+    if ((squares.includes("btn1") && squares.includes("btn2") && squares.includes("btn3")) ||
+        (squares.includes("btn4") && squares.includes("btn5") && squares.includes("btn6")) ||
+        (squares.includes("btn7") && squares.includes("btn8") && squares.includes("btn9")) ||
+        (squares.includes("btn1") && squares.includes("btn4") && squares.includes("btn7")) ||
+        (squares.includes("btn2") && squares.includes("btn5") && squares.includes("btn8")) ||
+        (squares.includes("btn3") && squares.includes("btn6") && squares.includes("btn9")) ||
+        (squares.includes("btn1") && squares.includes("btn5") && squares.includes("btn9")) ||
+        (squares.includes("btn3") && squares.includes("btn5") && squares.includes("btn7"))) {//win conditions
+        document.getElementById("btn1").disabled = true;
+        document.getElementById("btn2").disabled = true;
+        document.getElementById("btn3").disabled = true;
+        document.getElementById("btn4").disabled = true;
+        document.getElementById("btn5").disabled = true;
+        document.getElementById("btn6").disabled = true;
+        document.getElementById("btn7").disabled = true;
+        document.getElementById("btn8").disabled = true;
+        document.getElementById("btn9").disabled = true;
+        highlightSquares(squares);  
         if (turn) {
             document.getElementById("winner").innerHTML = "X Wins!";
-            //console.log("X Wins!");
         }
         else {
             document.getElementById("winner").innerHTML = "O Wins!";
         }
     }
-    if (document.getElementById("btn1").disabled == true &&
+    else if (document.getElementById("btn1").disabled == true &&
         document.getElementById("btn2").disabled == true && 
         document.getElementById("btn3").disabled == true &&
         document.getElementById("btn4").disabled == true &&
@@ -54,15 +63,15 @@ function checkWin(squares) {
 }
 
 function reset() {
-    document.getElementById("btn1").innerHTML = ".";
-    document.getElementById("btn2").innerHTML = ".";
-    document.getElementById("btn3").innerHTML = ".";
-    document.getElementById("btn4").innerHTML = ".";
-    document.getElementById("btn5").innerHTML = ".";
-    document.getElementById("btn6").innerHTML = ".";
-    document.getElementById("btn7").innerHTML = ".";
-    document.getElementById("btn8").innerHTML = ".";
-    document.getElementById("btn9").innerHTML = ".";
+    document.getElementById("btn1").innerHTML = "&nbsp";
+    document.getElementById("btn2").innerHTML = "&nbsp";
+    document.getElementById("btn3").innerHTML = "&nbsp";
+    document.getElementById("btn4").innerHTML = "&nbsp";
+    document.getElementById("btn5").innerHTML = "&nbsp";
+    document.getElementById("btn6").innerHTML = "&nbsp";
+    document.getElementById("btn7").innerHTML = "&nbsp";
+    document.getElementById("btn8").innerHTML = "&nbsp";
+    document.getElementById("btn9").innerHTML = "&nbsp";
     document.getElementById("btn1").disabled = false;
     document.getElementById("btn2").disabled = false;
     document.getElementById("btn3").disabled = false;
@@ -72,9 +81,60 @@ function reset() {
     document.getElementById("btn7").disabled = false;
     document.getElementById("btn8").disabled = false;
     document.getElementById("btn9").disabled = false;
+    document.getElementById("btn1").style.backgroundColor = "#FFFFFF";
+    document.getElementById("btn2").style.backgroundColor = "#FFFFFF";
+    document.getElementById("btn3").style.backgroundColor = "#FFFFFF";
+    document.getElementById("btn4").style.backgroundColor = "#FFFFFF";
+    document.getElementById("btn5").style.backgroundColor = "#FFFFFF";
+    document.getElementById("btn6").style.backgroundColor = "#FFFFFF";
+    document.getElementById("btn7").style.backgroundColor = "#FFFFFF";
+    document.getElementById("btn8").style.backgroundColor = "#FFFFFF";
+    document.getElementById("btn9").style.backgroundColor = "#FFFFFF";    
     Xsquares = [];
     Osquares = [];
     turn = true;
     document.getElementById("winner").innerHTML = "It is X's turn";
 }
 
+function highlightSquares(squares) {
+    if (squares.includes("btn1") && squares.includes("btn2") && squares.includes("btn3")) {
+        document.getElementById("btn1").style.backgroundColor = "green";
+        document.getElementById("btn2").style.backgroundColor = "green";
+        document.getElementById("btn3").style.backgroundColor = "green";
+    }
+    else if (squares.includes("btn4") && squares.includes("btn5") && squares.includes("btn6")) {
+        document.getElementById("btn4").style.backgroundColor = "green";
+        document.getElementById("btn5").style.backgroundColor = "green";
+        document.getElementById("btn6").style.backgroundColor = "green";
+    }
+    else if (squares.includes("btn7") && squares.includes("btn8") && squares.includes("btn9")) {
+        document.getElementById("btn7").style.backgroundColor = "green";
+        document.getElementById("btn8").style.backgroundColor = "green";
+        document.getElementById("btn9").style.backgroundColor = "green";
+    }
+    else if (squares.includes("btn1") && squares.includes("btn4") && squares.includes("btn7")) {
+        document.getElementById("btn1").style.backgroundColor = "green";
+        document.getElementById("btn4").style.backgroundColor = "green";
+        document.getElementById("btn7").style.backgroundColor = "green";
+    }
+    else if (squares.includes("btn2") && squares.includes("btn5") && squares.includes("btn8")) {
+        document.getElementById("btn2").style.backgroundColor = "green";
+        document.getElementById("btn5").style.backgroundColor = "green";
+        document.getElementById("btn8").style.backgroundColor = "green";
+    }
+    else if (squares.includes("btn3") && squares.includes("btn6") && squares.includes("btn9")) {
+        document.getElementById("btn3").style.backgroundColor = "green";
+        document.getElementById("btn6").style.backgroundColor = "green";
+        document.getElementById("btn9").style.backgroundColor = "green";
+    }
+    else if (squares.includes("btn1") && squares.includes("btn5") && squares.includes("btn9")) {
+        document.getElementById("btn1").style.backgroundColor = "green";
+        document.getElementById("btn5").style.backgroundColor = "green";
+        document.getElementById("btn9").style.backgroundColor = "green";
+    }
+    else if (squares.includes("btn3") && squares.includes("btn5") && squares.includes("btn7")) {
+        document.getElementById("btn3").style.backgroundColor = "green";
+        document.getElementById("btn5").style.backgroundColor = "green";
+        document.getElementById("btn7").style.backgroundColor = "green";
+    }
+}
